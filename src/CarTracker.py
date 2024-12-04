dcfece712127b46d74dbaf9eec74303020bad377
Jan Boruń <76433910+Ymir41@users.noreply.github.com>
2024-12-04 21:07:23 +0100

    dodanie CarTracker i SpotTracker

---
+++ src/CarTracker.py
@@ -0,0 +1,5 @@
+from Tracker import Tracker
+
+class CarTracker(Tracker):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ src/SpotTracker.py
@@ -0,0 +1,5 @@
+from Tracker import Tracker
+
+class SpotTracker(Tracker):
+    def __init__(self) -> None:
+        super().__init__()
