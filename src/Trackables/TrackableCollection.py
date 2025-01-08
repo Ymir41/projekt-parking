b81178c3b226c747713e7fd0cae8bdbdf1d19f80
Jan Boruń <76433910+Ymir41@users.noreply.github.com>
2025-01-08 17:57:58 +0100

    Dodano dokumentacje i poprawiono typy w TrackableCollection

--- src/Trackables/TrackableCollection.py
+++ src/Trackables/TrackableCollection.py
@@ -2,9 +2,15 @@ from src.Trackables.Trackable import Trackable, Box
 import numpy as np
 
 class TrackableCollection(object):
-    def trackables(self) -> list[tuple[any, any]]:
+    def trackables(self) -> list[tuple[int|str, Box]]: # implemented in each class
         pass
 
-    def draw(self, img: np.ndarray) -> np.ndarray:
+    def draw(self, img: np.ndarray, colors: dict[str, tuple[float, float, float]]) -> np.ndarray:
+        """
+        Draws a box with label on top on provided opencv image. For each label uses color from dictionary colors.
+        :param img: np.ndarray with image (B,G,R). An image on witch it will draw.
+        :param colors: dictionary with colors for each label - colors as tuple in BGR format
+        :return: np.ndarray an image with boxes of that TrackableCollection drawn on.
+        """
         for label, box in self.trackables():
             pass
