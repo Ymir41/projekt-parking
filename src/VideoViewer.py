00687560a13bbcdf70780104e82519bbcb7a67c8
romeq41 <jakubromanowski41@gmail.com>
2025-01-11 17:57:19 +0100

    Dodanie wyświetlania okna

--- src/VideoViewer.py
+++ src/VideoViewer.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 
+
 class VideoViewer(object):
     """
     A class used to display video.
     """
-    def __init__(self, name:str) -> None:
+
+    def __init__(self, name: str) -> None:
         """
         :param name: name of window.
         """
@@ -14,10 +16,12 @@ class VideoViewer(object):
     def __del__(self):
         cv2.destroyWindow(self.name)
 
-    def displayFrame(self, frame:np.ndarray) -> None:
+    def displayFrame(self, frame: np.ndarray) -> None:
         """
         Displays a single frame of the video.
         :param frame: new frame to be diplayed
         :return:
         """
+        cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(self.name, 600, 400)
         cv2.imshow(self.name, frame)
